ec91e74baa8f843946dfed362a89e139c76f8a51
Adrian Moya <adrianmoya@gmail.com>
2012-06-13 20:16:01 -0430

    Adding initial tests files

---
+++ Tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+require_once $_SERVER['SYMFONY'].'/Symfony/Component/HttpFoundation/UniversalClassLoader.php';
+
+use Symfony\Component\HttpFoundation\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
+$loader->register();
